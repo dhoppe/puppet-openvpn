c021b9a0f8f33631cd243edcaef09eb2516b1e8c
Dennis Hoppe <github@debian-solutions.de>
2016-11-04 21:18:35 +0100

    Fix several Rubocop issues

--- spec/defines/define_spec.rb
+++ spec/defines/define_spec.rb
@@ -1,78 +1,88 @@
 require 'spec_helper'
 
-describe 'openvpn::define', :type => :define do
+describe 'openvpn::define', type: :define do
   ['Debian'].each do |osfamily|
-    let(:facts) {{
-      :osfamily          => osfamily,
-      :ipaddress_primary => '10.0.2.15',
-    }}
+    let(:facts) do
+      {
+        osfamily: osfamily,
+        ipaddress_primary: '10.0.2.15'
+      }
+    end
     let(:pre_condition) { 'include openvpn' }
     let(:title) { 'openvpn.conf' }
 
     context "on #{osfamily}" do
       context 'when source file' do
-        let(:params) {{
-          :config_file_path   => '/etc/openvpn/openvpn.2nd.conf',
-          :config_file_source => 'puppet:///modules/openvpn/common/etc/openvpn/openvpn.conf',
-        }}
+        let(:params) do
+          {
+            config_file_path: '/etc/openvpn/openvpn.2nd.conf',
+            config_file_source: 'puppet:///modules/openvpn/common/etc/openvpn/openvpn.conf'
+          }
+        end
 
         it do
           is_expected.to contain_file('define_openvpn.conf').with(
             'ensure'  => 'present',
             'source'  => 'puppet:///modules/openvpn/common/etc/openvpn/openvpn.conf',
             'notify'  => 'Service[openvpn]',
-            'require' => 'Package[openvpn]',
+            'require' => 'Package[openvpn]'
           )
         end
       end
 
       context 'when content string' do
-        let(:params) {{
-          :config_file_path   => '/etc/openvpn/openvpn.3rd.conf',
-          :config_file_string => '# THIS FILE IS MANAGED BY PUPPET',
-        }}
+        let(:params) do
+          {
+            config_file_path: '/etc/openvpn/openvpn.3rd.conf',
+            config_file_string: '# THIS FILE IS MANAGED BY PUPPET'
+          }
+        end
 
         it do
           is_expected.to contain_file('define_openvpn.conf').with(
             'ensure'  => 'present',
-            'content' => /THIS FILE IS MANAGED BY PUPPET/,
+            'content' => %r{THIS FILE IS MANAGED BY PUPPET},
             'notify'  => 'Service[openvpn]',
-            'require' => 'Package[openvpn]',
+            'require' => 'Package[openvpn]'
           )
         end
       end
 
       context 'when content template' do
-        let(:params) {{
-          :config_file_path     => '/etc/openvpn/openvpn.4th.conf',
-          :config_file_template => 'openvpn/common/etc/openvpn/openvpn.conf.erb',
-        }}
+        let(:params) do
+          {
+            config_file_path: '/etc/openvpn/openvpn.4th.conf',
+            config_file_template: 'openvpn/common/etc/openvpn/openvpn.conf.erb'
+          }
+        end
 
         it do
           is_expected.to contain_file('define_openvpn.conf').with(
             'ensure'  => 'present',
-            'content' => /THIS FILE IS MANAGED BY PUPPET/,
+            'content' => %r{THIS FILE IS MANAGED BY PUPPET},
             'notify'  => 'Service[openvpn]',
-            'require' => 'Package[openvpn]',
+            'require' => 'Package[openvpn]'
           )
         end
       end
 
       context 'when content template (custom)' do
-        let(:params) {{
-          :config_file_path         => '/etc/openvpn/openvpn.5th.conf',
-          :config_file_template     => 'openvpn/common/etc/openvpn/openvpn.conf.erb',
-          :config_file_options_hash => {
-            'key' => 'value',
-          },
-        }}
+        let(:params) do
+          {
+            config_file_path: '/etc/openvpn/openvpn.5th.conf',
+            config_file_template: 'openvpn/common/etc/openvpn/openvpn.conf.erb',
+            config_file_options_hash: {
+              'key' => 'value'
+            }
+          }
+        end
 
         it do
           is_expected.to contain_file('define_openvpn.conf').with(
             'ensure'  => 'present',
-            'content' => /THIS FILE IS MANAGED BY PUPPET/,
+            'content' => %r{THIS FILE IS MANAGED BY PUPPET},
             'notify'  => 'Service[openvpn]',
-            'require' => 'Package[openvpn]',
+            'require' => 'Package[openvpn]'
           )
         end
       end
